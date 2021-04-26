3dc4b0962702a143505f2511e2b796a9ece123b5
Brydon <brydonti@gmail.com>
2021-04-26 17:31:44 +0800

    Attempt to fix code

--- src/screens/MCQ.js
+++ src/screens/MCQ.js
@@ -1,64 +1,68 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState,useEffect } from "react";
 import { StyleSheet, View, Text, AsyncStorage, ImageBackground } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { cos } from "react-native-reanimated";
 import { ReloadInstructions } from "react-native/Libraries/NewAppScreen";
 import { Lesson } from "../../App";
-const data = require('../Data/MCQ.json').questions
+const MCQdata = require('../Data/MCQ.json').questions
 
+function grabData(data,PrimaryTypeA, TopicNameA){
+  var quesData = []
+    for (var i = 0; i <= data.length-1; i++){
+      if (data[i][`upper/lowerPrimary`].toLowerCase() == PrimaryTypeA.toLowerCase() && (data[i].overallTopic).startsWith(TopicNameA)){
+        var usedPos = []
+        var pos;
+        var options = [0,0,0,0]
+        var counter = 0
+        var correctAns;
+        
+              
+        while(usedPos.length < 4){
+          pos =  Math.floor((Math.random() * 4))
+               
+          if (!usedPos.includes(pos)) {
+            usedPos.push(pos)
+            if (counter < 3){
+              options[pos] = data[i][`option${counter+1}`]
+            }else{
+              options[pos] = data[i].answers
+              correctAns = pos
+            }
+            counter += 1
+          }
+        }
+           
+        quesData.push({"Q":data[i].questions,
+                            "1":options[0],
+                            "2":options[1],
+                            "3":options[2],
+                            "4":options[3],
+                            "correctOption": correctAns})
+      }
+  }
+  return quesData
+}
 function MCQ({navigation, route}) {
+    
     const {PrimaryType} = route.params;
     const {TopicName} = route.params;
-    var quesData = []
-    var totalQ = 0
-    for (var i = 0; i <= data.length-1; i++){
-        if (data[i][`upper/lowerPrimary`].toLowerCase() == PrimaryType.toLowerCase() && (data[i].overallTopic).startsWith(TopicName)){
-            var usedPos = []
-            var pos;
-            var options = [0,0,0,0]
-            var counter = 0
-            var correctAns;
-            totalQ += 1
-            console.log(totalQ)
-            while(usedPos.length < 4){
-               pos =  Math.floor((Math.random() * 4))
-               
-               if (!usedPos.includes(pos)) {
-                   usedPos.push(pos)
-                   if (counter < 3){
-                    options[pos] = data[i][`option${counter+1}`]
-                   }else{
-                    options[pos] = data[i].answers
-                    correctAns = pos
-                    
-                     
-                    
 
-                   }
-                   
-                   counter += 1
-                }
-              }
-           
-          quesData.push({"Q":data[i].questions,
-                          "1":options[0],
-                          "2":options[1],
-                          "3":options[2],
-                          "4":options[3],
-                          "correctOption": correctAns})
-                        }
-                      }
-
-            console.log(quesData)
-    
-    
+    const [mcqData, setmcqData] = useState([])
     const[currquesNo, setcurrquesNo] = useState(0)
     const[colourop, setcolourop] = useState(["rgba(204,198,198,1)","rgba(204,198,198,1)","rgba(204,198,198,1)","rgba(204,198,198,1)"])
     
+    useEffect(() => {
+      setmcqData(grabData(MCQdata,PrimaryType,TopicName))
+
+
+    }, [])
+    
+   
     function checkAns(ans){
       var colourList = [...colourop]
       
-      var ohighlightR = quesData[currquesNo].correctOption
-      if (ans == quesData[currquesNo].correctOption){
+      var ohighlightR = mcqData[currquesNo].correctOption
+      if (ans == mcqData[currquesNo].correctOption){
         
         colourList[ans] = "rgb(144, 205, 183)"
           
@@ -70,7 +74,7 @@ function MCQ({navigation, route}) {
       }
       setcolourop(colourList)
       
-      console.log(colourop)
+      
       
 
   
@@ -89,7 +93,7 @@ function MCQ({navigation, route}) {
     var style4 = {
       backgroundColor: colourop[3]
       }
-      console.log(style1)
+      
     return (
         <View style={styles.container}>
           <ImageBackground
@@ -103,24 +107,24 @@ function MCQ({navigation, route}) {
         </ImageBackground>
 
         <View style={styles.rect1}>
-          <Text style={styles.loremIpsum}>{currquesNo}/{totalQ}</Text>
+          <Text style={styles.loremIpsum}>{currquesNo}/10</Text>
       </View>
       <View style={styles.rect}>
-        <Text style={styles.question}>{quesData[currquesNo].Q}</Text>
+        <Text style={styles.question}>{mcqData[currquesNo].Q}</Text>
       </View>
       
       <TouchableOpacity onPress={() => checkAns(0)}
       style={[styles.rect2, style1]}>
-          <Text style={styles.option1}>{quesData[currquesNo]["1"]}</Text>
+          <Text style={styles.option1}>{mcqData[currquesNo]["1"]}</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => checkAns(1)} style={[styles.rect3,style2]}>
-         <Text style={styles.option2}>{quesData[currquesNo]["2"]}</Text>
+         <Text style={styles.option2}>{mcqData[currquesNo]["2"]}</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => checkAns(2)} style={[styles.rect4,style3]}>
-         <Text style={styles.option3}>{quesData[currquesNo]["3"]}</Text>
+         <Text style={styles.option3}>{mcqData[currquesNo]["3"]}</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => checkAns(3)} style={[styles.rect5, style4]}>
-        <Text style={styles.option4}>{quesData[currquesNo]["4"]}</Text>
+        <Text style={styles.option4}>{mcqData[currquesNo]["4"]}</Text>
       </TouchableOpacity>
         </View>
 
